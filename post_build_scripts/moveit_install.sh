cf89da9c9cd3bce4328f9c27ba724728d343a82a
Dan M <daniel@thewindmills.com.au>
2024-05-28 12:50:11 +1000

    moveinstall change

--- post_build_scripts/moveit_install.sh
+++ post_build_scripts/moveit_install.sh
@@ -5,10 +5,11 @@ MARKER_FILE=$WORKSPACE/.moveit_install_done
 
 if [ ! -f "$MARKER_FILE" ]; then
     mkdir -p $WORKSPACE/src
-    cd $WORKSPACE/src
+    cd $WORKSPACE
     colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
     colcon mixin update default
     rosdep update
+    cd $WORKSPACE/src
     git clone --branch $ROS_DISTRO https://github.com/ros-planning/moveit2_tutorials
     vcs import < moveit2_tutorials/moveit2_tutorials.repos
     source /opt/ros/$ROS_DISTRO/setup.bash
