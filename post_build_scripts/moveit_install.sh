f8db6ee8766a1164fa6d74d3254a5168eb1e8e7c
Dan M <daniel@thewindmills.com.au>
2024-05-28 12:44:47 +1000

    Updated moveit script

--- post_build_scripts/moveit_install.sh
+++ post_build_scripts/moveit_install.sh
@@ -4,6 +4,7 @@ WORKSPACE=$HOME/.moveit_workspace
 MARKER_FILE=$WORKSPACE/.moveit_install_done
 
 if [ ! -f "$MARKER_FILE" ]; then
+    mkdir -p $WORKSPACE/src
     cd $WORKSPACE/src
     colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
     colcon mixin update default
