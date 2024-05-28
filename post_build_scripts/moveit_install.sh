6b473bd2191b43bdf3ad9d2629cf1035e4228aa6
Dan M <daniel@thewindmills.com.au>
2024-05-28 13:04:09 +1000

    Fixed moveit install.sh

--- post_build_scripts/moveit_install.sh
+++ post_build_scripts/moveit_install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-WORKSPACE=$HOME/.moveit_workspace
+MOVEIT_WORKSPACE=$HOME/.moveit_workspace
 
-mkdir -p $WORKSPACE/src
-cd $WORKSPACE
+mkdir -p $MOVEIT_WORKSPACE/src
+cd $MOVEIT_WORKSPACE
 colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
 colcon mixin update default
 rosdep update
-cd $WORKSPACE/src
+cd $MOVEIT_WORKSPACE/src
 git clone --branch $ROS_DISTRO https://github.com/ros-planning/moveit2_tutorials
 vcs import < moveit2_tutorials/moveit2_tutorials.repos
 source /opt/ros/$ROS_DISTRO/setup.bash
-cd $WORKSPACE
+cd $MOVEIT_WORKSPACE
 rosdep install -r --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
 colcon build --parallel-workers 2 --cmake-args -DCMAKE_BUILD_TYPE=Release
 touch $MARKER_FILE
 
 
-source $WORKSPACE/install/setup.bash
+source $MOVEIT_WORKSPACE/install/setup.bash
