0cf73838be6f1341b35216c23d2aa47c74cace89
Dan M <daniel@thewindmills.com.au>
2024-05-28 16:05:28 +1000

    Started driver script

---
+++ driver-start-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sources everything before running
+
+
+./docker-attach /bin/bash -c "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp && \
+    source /opt/ros/$ROS_DISTRO/setup.bash && \
+    source /home/${USERNAME}/workspace/install/setup.bash && \
+    source /home/${USERNAME}/.moveit_workspace/install/setup.bash && \
+    $1" 
