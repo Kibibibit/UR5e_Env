0e95d5ba8ec779df6f47b61e0fab4f95c7e61a7d
Dan M <daniel@thewindmills.com.au>
2024-05-29 16:45:42 +1000

    removed custom xacro files from launch scripts temporarily

--- workspace/driver_scripts/driver-moveit-start.sh
+++ workspace/driver_scripts/driver-moveit-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 RVIZ=${1:-true}
-ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=ur5e launch_rviz:=$RVIZ \
-    description_file:="$(find par_pkg)/home/rosuser/workspace/src/par_pkg/urdf/arm-with-joint.xacro"
+ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=ur5e launch_rviz:=$RVIZ
+
+#Need to set up proper parameters for this   
+#description_file:="$(find par_pkg)/home/rosuser/workspace/src/par_pkg/urdf/arm-with-joint.xacro"
--- workspace/driver_scripts/driver-ur-start.sh
+++ workspace/driver_scripts/driver-ur-start.sh
@@ -5,5 +5,7 @@ RVIZ=${1:-true}
 ros2 launch ur_robot_driver ur_control.launch.py \
     ur_type:=ur5e \
     robot_ip:=10.234.6.49 \
-    launch_rviz:=$RVIZ \
-    description_file:="$(find par_pkg)/home/rosuser/workspace/src/par_pkg/urdf/arm-with-joint.xacro"
+    launch_rviz:=$RVIZ
+
+#Need to set up proper parameters for this
+#description_file:="$(find par_pkg)/home/rosuser/workspace/src/par_pkg/urdf/arm-with-joint.xacro"
