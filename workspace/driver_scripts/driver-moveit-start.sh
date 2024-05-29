39cbe057cf908d28cc9e37ed599c0e47d3ceb1ef
Dan M <daniel@thewindmills.com.au>
2024-05-29 16:37:45 +1000

    Added urdf and xacro

--- workspace/driver_scripts/driver-moveit-start.sh
+++ workspace/driver_scripts/driver-moveit-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 RVIZ=${1:-true}
-ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=ur5e launch_rviz:=$RVIZ
+ros2 launch ur_moveit_config ur_moveit.launch.py ur_type:=ur5e launch_rviz:=$RVIZ \
+    description_file:="$(find par_pkg)/home/rosuser/workspace/src/par_pkg/urdf/arm-with-joint.xacro"
--- workspace/driver_scripts/driver-ur-start.sh
+++ workspace/driver_scripts/driver-ur-start.sh
@@ -2,4 +2,8 @@
 
 RVIZ=${1:-true}
 
-ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=10.234.6.49 launch_rviz:=$RVIZ
+ros2 launch ur_robot_driver ur_control.launch.py \
+    ur_type:=ur5e \
+    robot_ip:=10.234.6.49 \
+    launch_rviz:=$RVIZ \
+    description_file:="$(find par_pkg)/home/rosuser/workspace/src/par_pkg/urdf/arm-with-joint.xacro"
