2522e8f0213b6ce5a48b7d2212e3ab508a20d9da
LaskarisD <s3845221@student.rmit.edu.au>
2024-05-31 10:54:06 +1000

    code from yesterday machine, added new action and attempt to integrate moveit

--- workspace/src/move_it_action_server/src/move_to_pose_action_server.cpp
+++ workspace/src/move_it_action_server/src/move_to_pose_action_server.cpp
@@ -5,23 +5,26 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
-#include "action_tutorials_interfaces/action/fibonacci.hpp"
-// #include <moveit/move_group_interface/move_group_interface.h>
+// #include "action_tutorials_interfaces/action/fibonacci.hpp"
+#include <moveit/move_group_interface/move_group_interface.h>
+#include "geometry_msgs/msg/pose.hpp"
+#include "par_interfaces/action/set_pose.hpp"
+
  
 class MoveToPoseActionServer : public rclcpp::Node
 {
 public:
-    using Fibonacci = action_tutorials_interfaces::action::Fibonacci;
-    using GoalHandle = rclcpp_action::ServerGoalHandle<Fibonacci>;
+    using SetPose = par_interfaces::action::SetPose;
+    using GoalHandle = rclcpp_action::ServerGoalHandle<SetPose>;
 
     explicit MoveToPoseActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()) : Node("move_to_pose_action_server", options)
     {
         using namespace std::placeholders;
 
-        this->action_server_ = rclcpp_action::create_server<Fibonacci> 
+        this->action_server = rclcpp_action::create_server<SetPose>;
         (
             this, // node
-            "fibonacci", //action name
+            "SetPose", //action name
             std::bind(&MoveToPoseActionServer::handle_goal, this, _1, _2), //goal callback
             std::bind(&MoveToPoseActionServer::handle_cancel, this, _1),
             std::bind(&MoveToPoseActionServer::handle_accepted, this, _1)
@@ -29,14 +32,14 @@ public:
     }
 
 private:
-    rclcpp_action::Server<Fibonacci>::SharedPtr action_server_;
+    rclcpp_action::Server<SetPose>::SharedPtr action_server;
 
 
     rclcpp_action::GoalResponse handle_goal(
-        const rclcpp_action::GoalUUID & uuid,
-        std::shared_ptr<const Fibonacci::Goal> goal)
+        const rclcpp_action::GoalUUID & uuid
+        /*std::shared_ptr<const SetPose::Goal> goal*/)
     {
-        RCLCPP_INFO(this->get_logger(), "Recieved goal request %d", goal->order);
+        // RCLCPP_INFO(this->get_logger(), "Recieved goal request %", goal->goal_pose);
 
         (void)uuid;
 
@@ -59,38 +62,52 @@ private:
     void execute(const std::shared_ptr<GoalHandle> goal_handle)
     {
         RCLCPP_INFO(this->get_logger(), "Executing goal");
-        rclcpp::Rate loop_rate(1);
-        const auto goal = goal_handle->get_goal();
-        auto feedback = std::make_shared<Fibonacci::Feedback>();
-        auto & sequence = feedback->partial_sequence;
-        sequence.push_back(0);
-        sequence.push_back(1);
-        auto result = std::make_shared<Fibonacci::Result>();
+        // rclcpp::Rate loop_rate(1);
+        const auto goal = goal_handle->get_goal(); 
+        // auto feedback = std::make_shared<SetPose::Feedback>();
 
-        for (int i =1; (i < goal->order) && rclcpp::ok(); ++i)
+        auto result = std::make_shared<SetPose::Result>();
+
+        static const std::string PLANNING_GROUP = "ur5e_manipulator";
+
+        moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+
+        // moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+        const moveit::core::JointModelGroup* joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+        geometry_msgs::msg::Pose target_pose = goal;
+
+        move_group.setPoseTarget(target_pose);
+
+        moveit::planning_interface::MoveGroupInterface::Plan plan_msg;
+
+        bool plan_success = move_group.plan(plan_msg) == moveit::core::MoveItErrorCode::SUCCESS;
+
+        goal_handle->publish_feedback(plan_success);
+        RCLCPP_INFO(this->get_logger(), "Plan success");
+
+
+        if(plan_success)
         {
-            if(goal_handle->is_canceling())
+            if(move_group.move())
             {
-                result->sequence = sequence;
-                goal_handle->canceled(result);
-                RCLCPP_INFO(this->get_logger(), "Goal canceled");
-                return;
+                
+                RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+            } else
+            {
+                RCLCPP_ERROR(this->get_logger(), "Goal failed");
+
             }
-
-            sequence.push_back(sequence[i] + sequence[i - 1]);
-
-            goal_handle->publish_feedback(feedback);
-            RCLCPP_INFO(this->get_logger(), "Publish feedback");
-
-            loop_rate.sleep();
-        }
-        
-        if(rclcpp::ok())
+            
+        } else
         {
-            result->sequence;
-            goal_handle->succeed(result);
-            RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+            RCLCPP_ERROR(this->get_logger(), "Goal failed");
+
         }
+
+        result->final_pose;
+        goal_handle->succeed(result);
     }
 };
 
