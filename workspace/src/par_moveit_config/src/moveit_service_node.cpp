c1202f15d7dd723663cda32d3272b0997b2d318d
Dan M <daniel@thewindmills.com.au>
2024-06-03 14:32:36 +1000

    Fixed the error by actually binding the callback properly

--- workspace/src/par_moveit_config/src/moveit_service_node.cpp
+++ workspace/src/par_moveit_config/src/moveit_service_node.cpp
@@ -13,7 +13,7 @@ MoveitServiceNode::MoveitServiceNode(const rclcpp::NodeOptions & options) : Node
   using namespace std::placeholders;
   this->service= this->create_service<CurrentMoveitPose>(
     "par_get_current_moveit_pose",
-    &MoveitServiceNode::get_current_pose
+    std::bind(&MoveitServiceNode::get_current_pose, this, _1, _2)
   );
 
   this->move_group_interface = std::make_shared<MoveGroupInterface>(MoveGroupInterface(std::shared_ptr<rclcpp::Node>(this), "ur_manipulator"));
@@ -21,7 +21,7 @@ MoveitServiceNode::MoveitServiceNode(const rclcpp::NodeOptions & options) : Node
 }
 
 void MoveitServiceNode::get_current_pose(const std::shared_ptr<CurrentMoveitPose::Request> request, const std::shared_ptr<CurrentMoveitPose::Response> response) {
-  (void)request;  
+  (void)request;
   response->pose = this->move_group_interface->getCurrentPose().pose;
 }
 
