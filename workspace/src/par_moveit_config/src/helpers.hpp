7412aa91e2c233a3cec2dcf3d25f4c5ba47acd85
Dan M <daniel@thewindmills.com.au>
2024-06-12 11:33:03 +1000

    Added in some rounding

--- workspace/src/par_moveit_config/src/helpers.hpp
+++ workspace/src/par_moveit_config/src/helpers.hpp
@@ -6,6 +6,8 @@
 #include "geometry_msgs/msg/pose.hpp"
 #include "geometry_msgs/msg/quaternion.hpp"
 
+#define ROUNDING_PLACES 6
+
 geometry_msgs::msg::Quaternion quaternion_from_rpy(double roll, double pitch, double yaw);
 geometry_msgs::msg::Vector3 rpy_from_quaternion(geometry_msgs::msg::Quaternion q);
 geometry_msgs::msg::Pose pose_from_waypoint_pose(par_interfaces::msg::WaypointPose waypoint_pose);
@@ -13,5 +15,7 @@ par_interfaces::msg::WaypointPose waypoint_pose_from_pose(geometry_msgs::msg::Po
 bool equal_approx(double a, double b, double margin);
 bool will_translate(par_interfaces::msg::WaypointPose a, par_interfaces::msg::WaypointPose b, double margin);
 double wrap_value(double min, double max, double value);
-
+double round_to_n_places(double n, double value);
+geometry_msgs::msg::Pose round_pose_to_n_places(double n, geometry_msgs::msg::Pose pose);
+par_interfaces::msg::WaypointPose round_waypoint_pose_to_n_places(double n, par_interfaces::msg::WaypointPose pose);
 #endif
