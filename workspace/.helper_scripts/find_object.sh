66e35c65653994724caa06345a9975ea326c0d65
Dan M <daniel@thewindmills.com.au>
2024-06-14 12:54:49 +1000

    Updated launch script info topic"

--- workspace/.helper_scripts/find_object.sh
+++ workspace/.helper_scripts/find_object.sh
@@ -6,4 +6,4 @@ ros2 launch find_object_2d find_object_3d.launch.py \
     objects_path:="/home/rosuser/workspace/src/par_pkg/objects/" \
     rgb_topic:=/camera/camera/color/image_raw \
     depth_topic:=/camera/camera/depth/image_rect_raw \
-    camera_info_topic:=/camera/camera/color/info
+    camera_info_topic:=/camera/camera/color/camera_info
