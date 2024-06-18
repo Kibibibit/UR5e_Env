5accb110e2a9e860da592ce0ac5e7198aeab73f6
Dan M <daniel@thewindmills.com.au>
2024-06-18 12:12:54 +1000

    Started adding some more control logic

--- workspace/src/par_pkg/par_pkg/main_controller_node.py
+++ workspace/src/par_pkg/par_pkg/main_controller_node.py
@@ -1,41 +1,44 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, Point
 from .connect4.connect4_client import Connect4Client
 from enum import Enum
-# from par_interfaces.action import PickAndPlace
+from par_interfaces.action import PickAndPlace
+from par_interfaces.msg import GamePieces, GamePiece, IVector2
 from std_msgs.msg import Bool
 from rclpy.qos import ReliabilityPolicy, QoSProfile
+from rclpy.action import ActionClient
+from rclpy.task import Future
+from rclpy.action.client import ClientGoalHandle
 
-
-class States(Enum):
-    FINDING_GRID: int = 0
+class State(Enum):
+    FINDING_GRID = 0
     """
     The game has just started, and the robot needs to find the grid.
     This hopefully will have been manually aligned before starting the program.
     """
-    WAITING_FOR_HUMAN:int = 1
+    WAITING_FOR_HUMAN = 1
     """
     We're waiting for the human's move.
     """
-    SIMULATING_GRAVITY:int = 2
+    SIMULATING_GRAVITY = 2
     """
     The human has placed their block, and gravity now needs to affect it
     """
-    HUMAN_MADE_INVALID_MOVE: int = 3
+    HUMAN_MADE_INVALID_MOVE = 3
     """
     The human tried to place their block in an invalid position, and it needs to be removed
     """
-    ROBOT_FINDING_PIECE: int = 4
+    ROBOT_FINDING_PIECE = 4
     """
     The robot is looking for a new piece to grab
     """
-    ROBOT_TURN: int = 5
+    ROBOT_TURN = 5
     """
     It's currently the robot's turn
     """
 
-BOARD_FRAME_ID = "object_board"
+GRIPPER_WIDTH = 16.0
 
 class MainControllerNode(Node):
 
@@ -43,10 +46,11 @@ class MainControllerNode(Node):
         super().__init__('main_controller_node')
 
 
-        # self.__pick_and_place_client = self.create_client(
-        #     PickAndPlace,
-        #     "/par/pick_and_place"
-        # )
+        self.__pick_and_place_client = ActionClient(
+            self,
+            PickAndPlace,
+            "/par/pick_and_place"
+        )
         
         self.__board_detected_subscriber = self.create_subscription(
             Bool,
@@ -55,10 +59,17 @@ class MainControllerNode(Node):
             QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE)
         )
 
+        self.__piece_detected_subscriber = self.create_subscription(
+            GamePieces,
+            "/par/game_pieces",
+            self.__game_pieces_callback,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE)
+        )
+
         self.__board_detected = False
 
         self.__connect4client = Connect4Client()
-        self.__state = States.FINDING_GRID
+        self.__state = State.FINDING_GRID
 
 
         self.__grid_pose: Pose = None
@@ -79,13 +90,13 @@ class MainControllerNode(Node):
             self.get_logger().info("Waiting for action to finish")
             return
         match self.__state:
-            case States.FINDING_GRID:
+            case State.FINDING_GRID:
                 self.__finding_grid()
-            case States.WAITING_FOR_HUMAN:
+            case State.WAITING_FOR_HUMAN:
                 self.__waiting_for_human()
-            case States.SIMULATING_GRAVITY:
+            case State.SIMULATING_GRAVITY:
                 self.__simulating_gravity()
-            case States.HUMAN_MADE_INVALID_MOVE:
+            case State.HUMAN_MADE_INVALID_MOVE:
                 self.__human_made_invalid_move()
 
     
@@ -93,7 +104,7 @@ class MainControllerNode(Node):
         self.get_logger().info("Looking for grid!")
         if (self.__board_detected):
             self.get_logger().info("Found grid! Proceeding to next stage")
-            self.__state = States.WAITING_FOR_HUMAN
+            self.__state = State.WAITING_FOR_HUMAN
 
     def __waiting_for_human(self):
         self.get_logger().info("Waiting for human")
@@ -107,11 +118,11 @@ class MainControllerNode(Node):
 
         ## If the human made a valid move, we want to then simulate gravity for the piece
         if (self.__connect4client.valid_move(self.__human_column)):
-            self.__state = States.SIMULATING_GRAVITY
+            self.__state = State.SIMULATING_GRAVITY
             return
         ## Otherwise, we go to the invalid move
         else:
-            self.__state = States.HUMAN_MADE_INVALID_MOVE
+            self.__state = State.HUMAN_MADE_INVALID_MOVE
             return
 
 
@@ -141,6 +152,44 @@ class MainControllerNode(Node):
     def __board_detected_callback(self, msg: Bool):
         if (msg.data):
             self.__board_detected = True
+    
+    def __move_piece(self, piece: GamePiece, position: IVector2, next_state:State):
+        self.get_logger().info("Waiting for pick and place action server...")
+        self.__pick_and_place_client.wait_for_server()
+        self.get_logger().info(f"Found server! Moving piece at {piece.board_position.x},{piece.board_position.y} to {position.x}, {position.y}")
+
+
+        goal = PickAndPlace.Goal()
+
+        goal.gripper_width = GRIPPER_WIDTH
+        goal.start_point = piece.world_position
+        goal.end_point = self.__get_world_pos_from_board(position)
+
+        pick_and_place_future = self.__pick_and_place_client.send_goal_async(goal)
+        pick_and_place_future.add_done_callback(lambda future: self.__move_piece_done_callback(future, next_state))
+
+    def __get_world_pos_from_board(self, point: IVector2) -> Point:
+        return Point()
+
+    def __get_board_pos_from_world(self, point: Point) -> IVector2:
+        return IVector2()
+
+    def __move_piece_done_callback(self, future:Future, next_state: State):
+
+        move_goal:ClientGoalHandle = future.result()
+
+        if not move_goal.accepted:
+            self.get_logger().info("Pick and Place server rejected goal!") 
+            return
+        
+        self.get_logger().info(f"Pick and Place server accepted goal!")
+
+        self.__state = next_state
+
+
+
+    def __game_pieces_callback(self, msg: GamePieces):
+        pass
 
 def main(args=None):
     rclpy.init(args=args)
